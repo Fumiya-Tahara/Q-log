ba6298b024ad735c72bda98d4aef2437527ae858
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-03-11 18:26:03 +0900

    マイグレーション変更

--- database/migrations/2023_03_11_143642_add_year_and_term_to_courses_table.php
+++ database/migrations/2023_03_11_143642_add_year_and_term_to_courses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('courses', function (Blueprint $table) {
+        Schema::table('classes', function (Blueprint $table) {
             $table->integer('year')->after('faculty');
             $table->string('term')->after('year');
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('courses', function (Blueprint $table) {
+        Schema::table('classes', function (Blueprint $table) {
             $table->dropColumn('year');
             $table->dropColumn('term');
         });
