199ca49ea9e893389a47ea9d9d1de46aa6cc86ae
gontawara <tungsten3414@gmail.com>
2023-03-11 16:55:08 +0900

    seed

--- database/seeders/ClassesSeeder.php
+++ database/seeders/ClassesSeeder.php
@@ -12,6 +12,35 @@ class ClassesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Classes::create([
+            'name' => '微分積分学Ⅰ',
+            'teacher' => '佐藤康彦',
+            'average_review' => '1',
+            'like_count' => '', 
+            'faculty' => '基幹教育', 
+            'year' => '2022', 
+            'term' => '前期', 
+            
+        ]);
+        Classes::create([
+            'name' => '線形代数学Ⅰ',
+            'teacher' => '佐々野詠淑',
+            'average_review' => '4.3',
+            'like_count' => '', 
+            'faculty' => '基幹教育', 
+            'year' => '2022', 
+            'term' => '前期', 
+            
+        ]);
+        Classes::create([
+            'name' => '力学基礎',
+            'teacher' => '坂東麻衣',
+            'average_review' => '3.6',
+            'like_count' => '', 
+            'faculty' => '基幹教育', 
+            'year' => '2022', 
+            'term' => '前期', 
+            
+        ]);
     }
 }
