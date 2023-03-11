bb631d78e82ebc561e88e630957b84d9e523ebd3
gontawara <tungsten3414@gmail.com>
2023-03-11 17:41:09 +0900

    seeder

--- database/seeders/CoursesSeeder.php
+++ database/seeders/CoursesSeeder.php
@@ -4,41 +4,42 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Course;
 
-class ClassesSeeder extends Seeder
+class CoursesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Classes::create([
+        Course::create([
             'name' => '微分積分学Ⅰ',
             'teacher' => '佐藤康彦',
-            'average_review' => '1',
-            'like_count' => '', 
+            'average_review' => 1,
+            'like_count' => 0, 
             'faculty' => '基幹教育', 
-            'year' => '2022', 
+            'year' => 2022, 
             'term' => '前期', 
             
         ]);
-        Classes::create([
+        Course::create([
             'name' => '線形代数学Ⅰ',
             'teacher' => '佐々野詠淑',
-            'average_review' => '4.3',
-            'like_count' => '', 
+            'average_review' => 4,
+            'like_count' => 0, 
             'faculty' => '基幹教育', 
-            'year' => '2022', 
+            'year' => 2022, 
             'term' => '前期', 
             
         ]);
-        Classes::create([
+        Course::create([
             'name' => '力学基礎',
             'teacher' => '坂東麻衣',
-            'average_review' => '3.6',
-            'like_count' => '', 
+            'average_review' => 3,
+            'like_count' => 0, 
             'faculty' => '基幹教育', 
-            'year' => '2022', 
+            'year' => 2022, 
             'term' => '前期', 
             
         ]);
