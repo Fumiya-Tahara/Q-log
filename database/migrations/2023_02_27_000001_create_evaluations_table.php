c27dfec34fb5c443756656b9fd22238926bef1ce
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-05-10 12:00:53 +0900

    外部キー制約の設定

--- database/migrations/2023_02_27_000001_create_evaluations_table.php
+++ database/migrations/2023_02_27_000001_create_evaluations_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
-            $table->integer('class_id');
-            $table->integer('user_id');
+            $table->foreignId('class_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->integer('review');
             $table->string('sentence')->nullable();
             $table->timestamps();
